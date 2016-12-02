7315287ef0494191543e12e49499c0148c0ef172
Raymond Zhong <raymond.century21@gmail.com>
2016-12-02 13:38:00 -0800

    added BPM

---
+++ assignments/Rails/bpm/app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ActiveRecord::Base
+  belongs_to :blog
+  has_many :messages, dependent: :destroy
+  validates :content, presence: true
+  validates :title, presence: true, length: { minimum: 7 }
+end
